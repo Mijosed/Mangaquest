03796fcffb622c34a72c5a42a82e3eb657cd8b44
Joshua Grandville <joshua77130@live.fr>
2025-02-05 16:27:27 +0100

    save test

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,5 +9,3 @@ if (file_exists(dirname(__DIR__).'/config/bootstrap.php')) {
 } elseif (method_exists(Dotenv::class, 'bootEnv')) {
     (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
 }
-
-require dirname(__DIR__).'/vendor/autoload.php';
